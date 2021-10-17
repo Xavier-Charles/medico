534e4c919485024e727b828b2b2208e1fd5b3d08
Xharles <charlesnzubechukwu000@gmail.com>
2021-10-17 12:17:55 +0100

    hero and ui fixes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/header'
+import Hero from '../components/hero'
 
 const home = () => {
   return (
     <div>
       <Header />
+      <Hero />
       
     </div>
   )
