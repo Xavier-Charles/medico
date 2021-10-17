24cafd5f40bad2f72ec65110049b2d280c4dc124
Xharles <charlesnzubechukwu000@gmail.com>
2021-10-17 12:29:58 +0100

    loader

---
+++ src/components/healthRecord.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import LoaderCard from './loaderCard'
+
+const HealthRecord = () => {
+  return (
+    <div className="container mx-auto mt-20">
+      <LoaderCard />
+      <br />
+      <LoaderCard />
+      <br />
+      <LoaderCard />
+    </div>
+  );
+}
+
+export default HealthRecord
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/header'
 import Hero from '../components/hero'
+import HealthRecord from '../components/healthRecord'
 
 const home = () => {
   return (
     <div>
       <Header />
       <Hero />
+      <HealthRecord />
       
     </div>
   )
